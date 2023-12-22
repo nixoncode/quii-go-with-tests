02623492a427f7217caee92b706fc2d2b70acf25
Nixon Kosgei <nkosgey6@gmail.com>
2023-12-22 11:57:43 +0300

    added hello world

---
+++ 01-hello-world/hello.go
@@ -0,0 +1,12 @@
+// Package _1_hello_world
+// Created by GoLand.
+// User: nixon
+// Date: 22/12/2023
+// Time: 11:56
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world!")
+}
