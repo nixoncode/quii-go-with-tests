3b6ee6bfa71209035c4e038484cb216b42e4f436
Nixon Kosgei <nkosgey6@gmail.com>
2024-01-11 09:17:47 +0300

    added benchmark test

--- 03-iteration/repeat_test.go
+++ 03-iteration/repeat_test.go
@@ -15,3 +15,11 @@ func TestRepeat(t *testing.T) {
 		t.Errorf("expected %q but got %q", expect, got)
 	}
 }
+
+func BenchmarkRepeat(b *testing.B) {
+	{
+		for i := 0; i < b.N; i++ {
+			Repeat("a")
+		}
+	}
+}
